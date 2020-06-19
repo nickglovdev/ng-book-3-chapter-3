c95126a5effa16baa2302f614391bc498a5abab0
nickglovdev <nickglovdev@gmail.com>
2020-06-19 13:51:46 -0500

    Added the Pet Lighting exercise

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,13 @@
 import doctor from "./practice/doctors.js"
+import createPet from "./practice/pets.js"
 
 doctor
 console.log(doctor)
+
+let petList = []
+const newPetOne = petList.push(createPet("Jack", "Jack Russel"))
+const newPetTwo = petList.push(createPet("Puddles", "Cat"))
+const newPetThree = petList.push(createPet("Zeek", "Mutt"))
+console.log(petList)
+
+
---
+++ scripts/practice/pets.js
@@ -0,0 +1,9 @@
+const createPet = (name, breed) => {
+    const newPet = {
+        name: name,
+        breed: breed
+    }
+    return newPet
+} 
+
+export default createPet
