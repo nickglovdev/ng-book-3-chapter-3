26d41ffd1688e86ad97835acffa5ea6281a61180
nickglovdev <nickglovdev@gmail.com>
2020-06-20 12:33:34 -0500

    I have added the music row portion.

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,39 @@
 import doctor from "./practice/doctors.js"
 import createPet from "./practice/pets.js"
 
+//Bringing them in one by one. Forgot how to bring them in a group.
+import  { createJumpStop } from "./practice/musicRow.js"
+import { createChatten } from "./practice/musicRow.js"
+import  { createPolar } from "./practice/musicRow.js"
+
+//Doctors
 doctor
 console.log(doctor)
 
+
+//Pets
 let petList = []
 const newPetOne = petList.push(createPet("Jack", "Jack Russel"))
 const newPetTwo = petList.push(createPet("Puddles", "Cat"))
 const newPetThree = petList.push(createPet("Zeek", "Mutt"))
 console.log(petList)
+
+//Music Row
+let jumpStopList = []
+let chattenList = []
+let polarList = []
+
+const newArtistOne = chattenList.push(createChatten("Bruce Atikins", "Country", 23)) 
+const newArtistTwo = polarList.push(createPolar("Jensen Brown", "Pop", 20))
+const newArtistThree = jumpStopList.push(createJumpStop("Dre Funkz", "Funk", 25))
+const newArtistFour = jumpStopList.push(createJumpStop("Dusta Grimes", "Rap", 21))
+const newArstistFive = chattenList.push(createChatten("Barthalomew Danielson", "Bluegrass", 23))
+const newArtistSix = chattenList.push(createChatten("Avilee Dallas", "Country", 19))
+const newArstistSeven = polarList.push(createPolar("Aust Kinkaid", "Pop", 22))
+const newArtistEight = jumpStopList.push(createJumpStop("Loyonce Branis", "Rap", 27))
+
+console.log("Chatten: Country or Bluegrass", chattenList)
+console.log("JumpStop: Funk and Rap, jumpStopList", jumpStopList)
+console.log("Polar: Pop", polarList)
 
 
---
+++ scripts/practice/musicRow.js
@@ -0,0 +1,28 @@
+const createJumpStop = (name, genre, age) => {
+    const jumpArtist = {
+        name: name,
+        genre: genre,
+        age: age
+    }
+    return jumpArtist
+}
+
+const createChatten =  (name, genre, age ) => {
+    const chattenArtist = {
+        name: name,
+        genre: genre,
+        age: age
+    }
+    return chattenArtist
+}
+
+const createPolar = (name, genre, age) => {
+    const polarArtist = {
+        name: name,
+        genre: genre,
+        age: age
+    }
+    return polarArtist
+}
+
+export {createJumpStop, createChatten, createPolar}
